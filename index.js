d2c5e6800fac6298699449e42bd5e3f274f2ff82
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-01-07 19:17:41 +0700

    fix(color): change background color

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-const backgroundColor = '#002b36'
-const foregroundColor = '#839496'
-const cursorColor = 'rgba(181, 137, 0, 0.6)'
-const borderColor = 'transparent'
+// const backgroundColor = "#001B21";
+const foregroundColor = "#839496";
+// const cursorColor = 'rgba(181, 137, 0, 0.6)'
+const cursorColor = "#FFFFFF";
+const borderColor = "transparent";
 
 const colors = {
-  lightBlack:     '#586e75',
-  black:          '#073642',
-  lightGreen:     '#586e75',
-  lightYellow:    '#657b83',
-  lightBlue:      '#839496',
-  lightCyan:      '#93a1a1',
-  white:          '#eee8d5',
-  lightWhite:     '#fdf6e3',
-  yellow:         '#b58900',
-  lightRed:       '#cb4b16',
-  red:            '#dc322f',
-  magenta:        '#d33682',
-  lightMagenta:   '#6c71c4',
-  blue:           '#268bd2',
-  cyan:           '#2aa198',
-  green:          '#859900'
-}
+	lightBlack: "#586e75",
+	black: "#073642",
+	lightGreen: "#586e75",
+	lightYellow: "#657b83",
+	lightBlue: "#839496",
+	lightCyan: "#93a1a1",
+	white: "#eee8d5",
+	lightWhite: "#fdf6e3",
+	yellow: "#b58900",
+	lightRed: "#cb4b16",
+	red: "#dc322f",
+	magenta: "#d33682",
+	lightMagenta: "#6c71c4",
+	blue: "#268bd2",
+	cyan: "#2aa198",
+	green: "#859900",
+};
 
-
-
-exports.decorateConfig = config => {
-
-  return Object.assign({}, config, {
-    foregroundColor,
-    backgroundColor,
-    borderColor,
-    cursorColor,
-    colors,
-    termCSS: `
-      ${config.termCSS || ''}
+exports.decorateConfig = (config) => {
+	return Object.assign({}, config, {
+		foregroundColor,
+		backgroundColor,
+		borderColor,
+		cursorColor,
+		colors,
+		termCSS: `
+      ${config.termCSS || ""}
     `,
-    css: `
-      ${config.css || ''}
+		css: `
+      ${config.css || ""}
       * {
       	text-rendering: optimizeLegibility;
         font-weight: 500;
@@ -64,6 +62,6 @@ exports.decorateConfig = config => {
       .splitpane_divider {
       	background-color: #002028 !important;
       }
-    `
-  })
-}
+    `,
+	});
+};
